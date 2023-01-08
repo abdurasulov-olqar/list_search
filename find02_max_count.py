e26c1d4b291c69c752645b7e7bce9df4bb0b0542
olqarabdurasulov <abdurasulovolqar123@mail.com>
2023-01-08 08:57:58 +0500

    maximum number soni

--- find02_max_count.py
+++ find02_max_count.py
@@ -5,4 +5,8 @@ def find_max_count(data):
         data: list of numbers
     returns: count of maximum numbers in the list
     """
-    return 0
+    m = data[0]
+    for i in data:
+        if m<i:
+            m = i
+    return data.count(m)
