8c5a91268e6467fe6df77db8c84b29c60c166be2
olqarabdurasulov <abdurasulovolqar123@mail.com>
2023-01-08 09:32:25 +0500

    count of minimum numbers

--- find08_min_count.py
+++ find08_min_count.py
@@ -5,4 +5,8 @@ def find_min_count(data):
         data: list of numbers
     returns: count of minimum numbers in the list
     """
-    return 0
+    m = data[0]
+    for i in data:
+        if m > i :
+            m = i
+    return data.count(m)
