b05e5546acd994ff2ccd8f70966a6f6ded4effc4
olqarabdurasulov <abdurasulovolqar123@mail.com>
2023-01-08 09:47:27 +0500

    maximum even

--- find05_max_even.py
+++ find05_max_even.py
@@ -5,7 +5,7 @@ def find_max_even(data):
         data: list of numbers
     returns: maximum even number in the list
     """
-    m = data[0]
+    m = 0
     for i in data:
         if m<=i and i % 2 == 0:
             m = i
