6057b6aa6ff4d3fb6eda75606924b43b2ac8a0d5
olqarabdurasulov <abdurasulovolqar123@mail.com>
2023-01-08 09:15:08 +0500

    minimum number

--- find07_min.py
+++ find07_min.py
@@ -5,5 +5,8 @@ def find_min(data):
         data: list of numbers
     returns: minimum number in the list
     """
-
-    return 0
+    m = data[0]
+    for i in data:
+        if m > i:
+            m = i
+    return m
